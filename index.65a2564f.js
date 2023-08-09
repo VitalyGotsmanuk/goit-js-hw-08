31816b22e8392b912bdb2c768f91abde7f033501
VitalyGotsmanuk <129224886+VitalyGotsmanuk@users.noreply.github.com>
2023-08-09 17:08:46 +0000

    Deploying to gh-pages from @ VitalyGotsmanuk/goit-js-hw-08@74e5058ace7acf34f7b8df02e21564a689ec9f0e 🚀

--- index.65a2564f.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello WORLD"),console.log("Hello!!!");
-//# sourceMappingURL=index.65a2564f.js.map
--- index.bd745889.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello WORLD"),console.log("Hello!!!");
-//# sourceMappingURL=index.bd745889.js.map
